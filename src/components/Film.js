077a2fcc268eefab48020d31bb7ce91e68902992
cjscrimmy <craigscrimgeour@yahoo.com>
2020-07-06 19:22:00 +0100

    initial set up of film file and display message on screen to test

---
+++ src/components/Film.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class Film extends Component{
+    render(){
+        return(
+            <li className="film">
+                i am a film
+            </li>
+        )
+    }
+}
+
+export default Film;
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Film from "./Film";
 
 class FilmList extends Component {
     render(){
         return(
             <div className="film-list">
-                test
+                <Film/>
+                <Film/>
             </div>
         );
     }
